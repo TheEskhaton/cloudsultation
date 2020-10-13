f293da2e1a6facaaffff65e52a186d7fdb8ed1c4
TheEskhaton <TheEskhaton@gmail.com>
2020-10-13 21:25:44 +0200

    added title

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
-import {
-  Container,
-  Stack
-} from "@chakra-ui/core";
+import { Container, Heading, Stack } from "@chakra-ui/core";
 import Head from "next/head";
 import Service from "../components/Service";
 import servicesJson from "../data/services.json";
@@ -19,19 +16,21 @@ export default function Home({services}) {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Cloud Consultant</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container maxW="lg" centerContent>
-      <Stack m="8" spacing="4">
-        <Stack spacing="4">
-          {services.items.map((s, idx) => {
-            return (
-              <Service key={idx} service={s}></Service>
-            );
-          })}
+        <Stack m="8" spacing="4">
+          <Stack spacing="4">
+            <Heading color="purple.700" mb="2" alignSelf="center" as="h1">
+              Should you use it?
+            </Heading>
+            {services.items.map((s, idx) => {
+              return <Service key={idx} service={s}></Service>;
+            })}
+          </Stack>
         </Stack>
-      </Stack></Container>
+      </Container>
     </div>
   );
 }
