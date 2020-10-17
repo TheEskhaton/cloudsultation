b56de5678acfd1229b530f49779b24b97bb53357
TheEskhaton <TheEskhaton@gmail.com>
2020-10-17 20:40:33 +0200

    Improved search

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
-import { Container, Heading, Icon, Input, InputGroup, InputLeftElement, Stack } from "@chakra-ui/core";
+import {
+  Container,
+  Heading,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack
+} from "@chakra-ui/core";
 import Head from "next/head";
 import { useMemo, useState } from "react";
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from "react-icons/fi";
 import Service from "../components/Service";
 import servicesJson from "../data/services.json";
 
 export async function getStaticProps() {
   return {
-    props : {
-      services: servicesJson
-    }
-  }
+    props: {
+      services: servicesJson,
+    },
+  };
 }
 
 export default function Home({ services }) {
@@ -35,24 +43,27 @@ export default function Home({ services }) {
       <Container maxW="lg" centerContent>
         <Stack m="8" spacing="4">
           <Stack spacing="4">
-             <Heading color="purple.700" mb="2" alignSelf="center" as="h1">
+            <Heading 
+            textTransform="uppercase" 
+            color="purple.700" mb="2" fontSize="1.75rem" alignSelf="center" as="h1">
               Is it right for you?
             </Heading>
-            <InputGroup   alignSelf="center" type="text" 
-            maxW="md">
-            <InputLeftElement pointerEvents="none">
-              <Icon as={FiSearch} color="gray.300"></Icon>
-              </InputLeftElement> 
-            <Input value={searchText} 
-            onChange={(e) => setSearchText(e.target.value)} 
-          
-            placeholder="Name of cloud service"></Input>
+            <InputGroup alignSelf="center" type="text" maxW="md">
+              <InputLeftElement pointerEvents="none">
+                <Icon as={FiSearch} color="gray.300"></Icon>
+              </InputLeftElement>
+              <Input
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                placeholder="Name of cloud service"
+              ></Input>
             </InputGroup>
-            
+
             {filteredServices.map((s, idx) => {
               return <Service key={idx} service={s}></Service>;
             })}
-          </Stack></Stack>
+          </Stack>
+        </Stack>
       </Container>
     </div>
   );
