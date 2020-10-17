86a2f94dc5fb25a3a8ded11d9e77b12a25324176
TheEskhaton <TheEskhaton@gmail.com>
2020-10-17 20:30:11 +0200

    Added search above services

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { Container, Heading, Stack } from "@chakra-ui/core";
+import { Container, Heading, Icon, Input, InputGroup, InputLeftElement, Stack } from "@chakra-ui/core";
 import Head from "next/head";
+import { useMemo, useState } from "react";
+import { FiSearch } from 'react-icons/fi';
 import Service from "../components/Service";
 import servicesJson from "../data/services.json";
-
 
 export async function getStaticProps() {
   return {
@@ -12,7 +13,19 @@ export async function getStaticProps() {
   }
 }
 
-export default function Home({services}) {
+export default function Home({ services }) {
+  const [searchText, setSearchText] = useState(null);
+
+  const filteredServices = useMemo(() => {
+    if (searchText) {
+      return services.items.filter((s) => {
+        return s.displayName.toLowerCase().includes(searchText.toLowerCase());
+      });
+    }
+
+    return services.items;
+  }, [searchText, services]);
+
   return (
     <div>
       <Head>
@@ -22,14 +35,24 @@ export default function Home({services}) {
       <Container maxW="lg" centerContent>
         <Stack m="8" spacing="4">
           <Stack spacing="4">
-            <Heading color="purple.700" mb="2" alignSelf="center" as="h1">
-              Should you use it?
+             <Heading color="purple.700" mb="2" alignSelf="center" as="h1">
+              Is it right for you?
             </Heading>
-            {services.items.map((s, idx) => {
+            <InputGroup   alignSelf="center" type="text" 
+            maxW="md">
+            <InputLeftElement pointerEvents="none">
+              <Icon as={FiSearch} color="gray.300"></Icon>
+              </InputLeftElement> 
+            <Input value={searchText} 
+            onChange={(e) => setSearchText(e.target.value)} 
+          
+            placeholder="Name of cloud service"></Input>
+            </InputGroup>
+            
+            {filteredServices.map((s, idx) => {
               return <Service key={idx} service={s}></Service>;
             })}
-          </Stack>
-        </Stack>
+          </Stack></Stack>
       </Container>
     </div>
   );
